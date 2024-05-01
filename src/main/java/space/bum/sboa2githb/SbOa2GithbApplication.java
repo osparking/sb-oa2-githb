6c82264ff20a97bf83f20408d068c57ed73a5433
osparking <jbpark03@gmail.com>
2024-05-01 10:31:06 +0900

    봄부트 OAuth 2, github client

---
+++ src/main/java/space/bum/sboa2githb/SbOa2GithbApplication.java
@@ -0,0 +1,13 @@
+package space.bum.sboa2githb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbOa2GithbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbOa2GithbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/bum/sboa2githb/SbOa2GithbApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bum.sboa2githb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbOa2GithbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
